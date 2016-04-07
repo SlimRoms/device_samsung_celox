2d52e5289c70c1d0f5602ca530931aa0c6a1bdf1
Dan Cartier <nospamdan1@gmail.com>
2016-04-07 02:05:17 -0400

    celox: Candify...

--- candy.mk
+++ candy.mk
@@ -1,11 +1,13 @@
+# Inherit device configuration
 $(call inherit-product, device/samsung/celox/full_celox.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
+# Override AOSP build properties
 PRODUCT_DEVICE := celox
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
