79a3f32523990f89cd826a90f53e538671c619db
Dan Cartier <nospamdan1@gmail.com>
2016-04-15 00:00:30 -0400

    celox: Add build barrier for rootdir packages
    init.qcom.usb.rc is trying to build with other local sources
    
    Change-Id: I65df0dc08a305548dd4177d242a25b681c41722d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -24,3 +26,5 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+endif
